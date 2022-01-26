832fd32046f557a68c46ead1b0a88b4ebde05a25
Aleksey Lomov <zibloidix@gmail.com>
2022-01-26 11:13:58 +1100

    DEV-10 Реализован вывод логов в консоль о входящем запросе

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
 func main() {
@@ -17,6 +18,7 @@ func main() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	printlog(r)
 	fmt.Fprintf(w, "Hello %q", r.URL.Path)
 }
 
@@ -26,4 +28,12 @@ func getPort() string {
 		return ":" + port
 	}
 	return ":3000"
+}
+
+func printlog(r *http.Request) {
+	t := time.Now().Format(time.ANSIC)
+	m := r.Method
+	h := r.Host
+	p := r.URL.Path
+	fmt.Fprintf(os.Stdout, "[%s]\t%s\t%s\t%s\n", t, m, h, p)
 }
