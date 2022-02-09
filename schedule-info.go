eee14b1ad81fc66d6a4dd66655c03f3fb9d3b6d0
Aleksey Lomov <zibloidix@gmail.com>
2022-02-09 15:31:06 +1100

    DEV-36 1. В шаблон GetHouseCallScheduleInfo добавлены поля для рендера структуры ScheduleInfoResponse

--- schedule-info.go
+++ schedule-info.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	_ "database/sql"
 	_ "github.com/mattn/go-sqlite3"
 	"net/http"
@@ -32,12 +33,18 @@ func GetHouseCallScheduleInfo(w http.ResponseWriter, r *http.Request) {
 		//	w.WriteHeader(http.StatusInternalServerError)
 		//	templateFile = respBase + respErr02
 		//}
-		slots := []Slot{
-			{"id01", "time01", 120},
-			{"id02", "time02", 120},
-			{"id03", "time03", 120},
-			{"id04", "time04", 120},
+		scheduleResponse := ScheduleInfoResponse {
+				SessionID: "123123123123123123",
+				Schedule: Schedule{
+					Slots: []Slot{
+						{SlotID:"id01", VisitTime:"time01", Duration:120},
+						{SlotID:"id02", VisitTime:"time02", Duration:120},
+						{SlotID:"id03", VisitTime:"time03", Duration:120},
+						{SlotID:"id04", VisitTime:"time04", Duration:120},
+				},
+			},
 		}
+		fmt.Println(scheduleResponse)
 		data, err := os.ReadFile(templateFile)
 		if err != nil {
 			panic(err)
@@ -45,7 +52,7 @@ func GetHouseCallScheduleInfo(w http.ResponseWriter, r *http.Request) {
 		t := template.New("tmp")
 		t.Parse(string(data))
 		w.Header().Add("Content-Type", "application/xml")
-		t.Execute(w, slots)
+		t.Execute(w, scheduleResponse)
 	}
 
 }
