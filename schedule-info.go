b0a94bb250eaf5450b92d6884cb93ed407746fcd
Aleksey Lomov <zibloidix@gmail.com>
2022-02-09 11:22:56 +1100

    DEV-35 1. Добавлен обработчик шаблона для операции GetHouseCallScheduleInfo 2. Объявлены базовые структуры для формирования шаблона

--- schedule-info.go
+++ schedule-info.go
@@ -1,10 +1,51 @@
 package main
 
-import "net/http"
+import (
+	_ "database/sql"
+	_ "github.com/mattn/go-sqlite3"
+	"net/http"
+	"os"
+	"text/template"
+)
+
+type ScheduleInfoResponse struct {
+	SessionID string
+	Schedule Schedule 
+}
+
+type Schedule struct {
+	Slots []Slot
+}
+
+type Slot struct {
+	SlotID string
+	VisitTime string
+	Duration int
+}
 
 func GetHouseCallScheduleInfo(w http.ResponseWriter, r *http.Request) {
 	action := r.Header.Get("soapaction")
 	if action == "GetHouseCallScheduleInfo" {
-		sendData("GetHouseCallScheduleInfo: "+action, w)
+		templateFile := respBase + action + "/" + respOk
+		//envelope, err := ParseSoapEnvelope(r.Body)
+		//if err != nil {
+		//	w.WriteHeader(http.StatusInternalServerError)
+		//	templateFile = respBase + respErr02
+		//}
+		slots := []Slot{
+			{"id01", "time01", 120},
+			{"id02", "time02", 120},
+			{"id03", "time03", 120},
+			{"id04", "time04", 120},
+		}
+		data, err := os.ReadFile(templateFile)
+		if err != nil {
+			panic(err)
+		}
+		t := template.New("tmp")
+		t.Parse(string(data))
+		w.Header().Add("Content-Type", "application/xml")
+		t.Execute(w, slots)
 	}
+
 }
